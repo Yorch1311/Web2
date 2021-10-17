3e009e45fa52745617a9d7ae9d1cedac5b580e6a
Yorch1311 <jorgecam131199@outlook.com>
2021-10-17 16:45:01 -0600

    Archivos de  miBlog

---
+++ posts/angular-y-scully.md
@@ -0,0 +1,7 @@
+---
+title: Angular y Scully
+description: Como crear un blog con Angular y Scully
+published: true
+---
+
+# Angular y Scully
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ContactModule } from './contact/contact.module';
+import { ScullyLibModule } from '@scullyio/ng-lib';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    ContactModule,
+    ModalModule.forRoot(),
+    ScullyLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
