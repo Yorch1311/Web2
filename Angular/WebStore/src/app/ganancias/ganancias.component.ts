5fd350012c816e9dfcb680b6f5d8d80674ee845c
Yorch1311 <jorgecam131199@outlook.com>
2021-12-08 08:03:17 -0700

    version 1.0
    este ya tiene lo minimo, se le impplementaran cosas que todavia no sonn finales

--- Angular/WebStore/src/app/ganancias/ganancias.component.ts
+++ Angular/WebStore/src/app/ganancias/ganancias.component.ts
@@ -11,7 +11,6 @@ import { Inicio } from '../inicio';
   styleUrls: ['./ganancias.component.scss'],
 })
 export class GananciasComponent implements OnInit {
-
   email: string | null;
   ventas: Grafica[] = [];
 
@@ -19,27 +18,22 @@ export class GananciasComponent implements OnInit {
 
   constructor(private api: ApiMercadoService) {
     this.email = sessionStorage.getItem('email');
-    if(this.email == null){
-      location.href = "/";
+    if (this.email == null) {
+      location.href = '/';
     }
   }
-  tipo = "";
-  valor = "";
+  tipo = '';
+  valor = '';
 
-
-
-
-//Datos de prueba para la grafica
-  public lineChartData: ChartDataSets[] = [
-    { data: this.data, label: 'VENTAS' },
-  ];
+  //Datos de prueba para la grafica
+  public lineChartData: ChartDataSets[] = [];
   public lineChartLabels: Label[] = [];
 
   public lineChartOptions = {
     scaleShowVerticalLines: false,
-    responsive: true
+    responsive: true,
   };
-/*
+  /*
   public barChartOptions = {
     scaleShowVerticalLines: false,
     responsive: true
@@ -57,70 +51,82 @@ export class GananciasComponent implements OnInit {
   currentDate = new Date();
   mostrar = false;
 
-  tipoGrafica( tipo: string){
+  tipoGrafica(tipo: string) {
     this.tipo = tipo;
     this.mostrar = false;
-    this.valor = "";
+    this.valor = '';
   }
 
-  mes( mes: string){
+  mes(mes: string) {
     this.valor = mes;
     this.mostrar = false;
   }
 
-  anio( anio: string){
+  anio(anio: string) {
     this.valor = anio;
     this.mostrar = false;
   }
 
-  generar(){
+  generar() {
     this.mostrar = true;
   }
 
-  venta(){
-
+  venta() {
     //this.titulo(this.tipo, this.valor);
 
-    var body = { "tipo": this.tipo, "valor": this.valor};
-    this.api.getVentaC(body).subscribe(datos => {
+    var body = { tipo: this.tipo, valor: this.valor };
+    this.api.getVentaC(body).subscribe((datos) => {
       this.ventas = datos;
+      console.log(this.ventas);
+      var datosA = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      var datosM = [0, 0, 0, 0];
+
+      if (this.tipo == 'year') {
+        this.ventas.forEach((data) => {
+          var i = data.month;
+          datosA[i - 1] = datosA[i - 1] + data.total;
+          //console.log(datosA[i - 1]);
+        });
+        console.log(datosA);
+        this.data = datosA;
+        console.log(this.data);
+        this.lineChartLabels = [
+          'Enero',
+          'Febrero',
+          'Marzo',
+          'Abril',
+          'Mayo',
+          'Junio',
+          'Julio',
+          'Agosto',
+          'Septiembre',
+          'Octubre',
+          'Noviembre',
+          'Diciembre',
+        ];
+      } else if (this.tipo == 'month') {
+        this.ventas.forEach((data) => {
+          var i = data.day;
+          if (i < 31) {
+            datosM[3] = datosM[3] + data.total;
+          } else if (i < 22) {
+            datosM[2] = datosM[2] + data.total;
+          } else if (i < 15) {
+            datosM[1] = datosM[1] + data.total;
+          } else if (i < 8) {
+            datosM[0] = datosM[0] + data.total;
+          }
+        });
+        console.log(datosM);
+        this.data = datosM;
+        this.lineChartLabels = ['Semana 1', 'Semana ', 'Semana 3', 'Semana 4'];
+      }
+
+      this.lineChartData = [{ data: this.data, label: 'VENTAS' }]
+
+      this.mostrar = true;
     });
-
-    var datosA = [0,0,0,0,0,0,0,0,0,0,0,0];
-    var datosM = [0,0,0,0];
-
-    if(this.tipo == "year"){
-
-      this.ventas.forEach(data => {
-        var i = data.month;
-        datosA[i-1] = datosA[i-1] + data.total;
-        console.log(datosA[i-1]);
-      });
-      this.data = datosA;
-      this.lineChartLabels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-
-    }else if( this.tipo == "month"){
-
-      this.ventas.forEach(data => {
-        var i = data.day;
-        if(i < 31){
-          datosM[3] = datosM[3] + data.total;
-        }else if(i < 22){
-          datosM[2] = datosM[2] + data.total;
-        }else if(i < 15){
-          datosM[1] = datosM[1] + data.total;
-        }else if(i < 8){
-          datosM[0] = datosM[0] + data.total;
-        }
-      });
-      this.data = datosM;
-      this.lineChartLabels = ["Semana 1", "Semana ", "Semana 3", "Semana 4"];
-    }
-
-    this.mostrar = true;
   }
-
-
 
   ngOnInit(): void {}
 }
